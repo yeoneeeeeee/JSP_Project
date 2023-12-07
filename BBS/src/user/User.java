80a33b954656c7044710f1f4092cb91c670ec1fc
soyeon <137244427+yeoneeeeeee@users.noreply.github.com>
2023-12-07 16:07:40 +0900

    BBS

--- BBS/src/user/User.java
+++ BBS/src/user/User.java
@@ -1,27 +1,23 @@
 package user;
 
 public class User {
-
-	private String userId;
-	private String userPwd;
+	private String userID;
+	private String userPassword;
 	private String userName;
 	private String userGender;
 	private String userEmail;
 	
-	public User() {
-	} 
-
-	public String getUserId() {
-		return userId;
+	public String getUserID() {
+		return userID;
 	}
-	public void setUserId(String userId) {
-		this.userId = userId;
+	public void setUserID(String userID) {
+		this.userID = userID;
 	}
-	public String getUserPwd() {
-		return userPwd;
+	public String getUserPassword() {
+		return userPassword;
 	}
-	public void setUserPwd(String userPwd) {
-		this.userPwd = userPwd;
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
 	}
 	public String getUserName() {
 		return userName;
@@ -42,13 +38,5 @@ public class User {
 		this.userEmail = userEmail;
 	}
 	
-	public User(String userId, String userPwd, String userName, String userGender, String userEmail) {
-		super();
-		this.userId = userId;
-		this.userPwd = userPwd;
-		this.userName = userName;
-		this.userGender = userGender;
-		this.userEmail = userEmail;
-	}
 	
 }
